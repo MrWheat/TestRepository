3f431ff482ea89a49c30c385c8aa88200cea893d
MaiZi <>
2018-05-08 12:40:42 +0800

    add workspace

---
+++ MZGitTestDemo/MZGitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MZGitTestDemo
+//
+//  Created by MaiZi on 2018/5/8.
+//  Copyright © 2018年 MaiZi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MZGitTestDemo/MZGitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MZGitTestDemo
+//
+//  Created by MaiZi on 2018/5/8.
+//  Copyright © 2018年 MaiZi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
